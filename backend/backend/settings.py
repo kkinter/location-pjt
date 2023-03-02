2317455cf92c22696befa9121a3229c4ce67ae3f
kkinter <ifol1129@gmail.com>
2023-03-03 01:02:08 +0900

    Config | cors headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,12 +55,17 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'rest_framework_gis',
+    'corsheaders',
 
     'listings.apps.ListingsConfig',
     'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
+    # cors header
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    #
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,3 +153,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
