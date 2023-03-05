6397dc998215e8e6e65aaecdf8a8cb9b308fee55
kkinter <ifol1129@gmail.com>
2023-03-05 21:15:01 +0900

    Prac | loop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,16 @@ import "./App.css";
 // Compo
 import Testing from "./Components/Testing";
 import SecondComponent from "./Components/SecondComponent";
+import Fruit from "./Components/Fruit";
+
+const theFruits = [
+  { id: 1, name: "Apple", color: "red" },
+  { id: 2, name: "Orange", color: "orange" },
+  { id: 3, name: "Banana", color: "yellow" },
+  { id: 4, name: "Kiwi", color: "green" },
+  { id: 5, name: "Mango", color: "red" },
+  { id: 6, name: "Avocado", color: "green" },
+];
 
 function App() {
   function MyComponent() {
@@ -17,6 +27,15 @@ function App() {
   }
   return (
     <BrowserRouter>
+      {theFruits.map(fruit => {
+        // console.log(fruit, typeof fruit)
+        return (
+          <Fruit key={fruit.id} name={fruit.color} color={fruit.name} />
+          // <h5>
+          //   This is a {fruit.color} {fruit.name}
+          // </h5>
+        );
+      })}
       <MyComponent />
       <SecondComponent />
       <Routes>
